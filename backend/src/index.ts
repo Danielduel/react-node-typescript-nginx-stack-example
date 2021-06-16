98e831b8cce7ae9567606fd9ef7beb79cd949d4d
Daniel Mizerski <duelsik@gmail.com>
2021-06-16 19:08:48 +0200

    (tech) Clean project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,6 @@ import { LiveActivatedChannels } from "types/LiveActivatedChannels";
 
 const app = express();
 
-app.get("/list", (req, res) => {
-  res.send("a list");
-})
 app.get("/list-of-channels", (req, res) => {
   const extensionId = "u4pv564j7h1cq9bcuf9tk8yohixws4";
   fetch(`https://api.twitch.tv/extensions/${extensionId}/live_activated_channels`, {
@@ -16,10 +13,6 @@ app.get("/list-of-channels", (req, res) => {
     })
     .then(res => res.json() as Promise<LiveActivatedChannels>)
     .then(data => res.send(data));
-
-  // curl -H "Client-Id: u4pv564j7h1cq9bcuf9tk8yohixws4" \
-  // -X GET https://api.twitch.tv/extensions/u4pv564j7h1cq9bcuf9tk8yohixws4/live_activated_channels
-  // res.send("Hello!");
 });
 app.listen(80, () => {
   console.log("Express app is listening");
