a5d51ff5da45b61315ac278704ce4964e6730150
Daniel Mizerski <duelsik@gmail.com>
2021-06-21 22:10:41 +0200

    Revert docker stack support.

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-echo ================ Create registry ================
-docker service create --name registry --publish published=5000,target=5000 registry:2
-echo ================ Compose build ================
-docker-compose -f docker-stack.yml build
-echo ================ Stack deploy ================
-docker stack deploy -c docker-stack.yml demo
